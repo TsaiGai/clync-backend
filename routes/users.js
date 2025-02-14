8346ef9a302b4a4bd3624115721cd062375cee98
TsaiGai <iantsai11267@gmail.com>
2025-02-14 01:14:55 -0800

    Fix routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
-const Apartment = require("../models/Apartment");
+const userController = require("../controllers/userController");
 
 // Get all apartments for a user (with userController)
-router.get("/:userId/apartments", getUserApartments);
+router.get("/:userId/apartments", userController.getUserApartments);
 
 module.exports = router;
